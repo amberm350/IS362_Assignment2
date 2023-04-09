c127ccd12c8634d1d1ee27a50d6fcdb07605a8a1
amberm350 <112363236+amberm350@users.noreply.github.com>
2023-04-09 14:39:33 -0400

    Add files via upload

---
+++ AMohsin_IS362_Assignment2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
+#IS362 Assignment 2
+newdictionary = {
+    "teacher" : "Mr. M",
+    "class" : "6A",
+    "room": 102,
+    "grade level" : 6
+}
+
+newdictionary["teacher"] = "Ms. L"
+print(newdictionary)
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
